7e794e00cbf60cd18d73e38ff9db94e2118a737d
onesmus <onesmus1024@gmail.com>
2022-06-11 13:12:22 +0300

    simple fix

--- src/main/java/com/example/wambugugrocery/controllers/ProductController.java
+++ src/main/java/com/example/wambugugrocery/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package com.example.wambugugrocery.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
